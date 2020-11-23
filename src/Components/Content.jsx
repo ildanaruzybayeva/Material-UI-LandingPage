7c5de49ca7ab51dd5fb0811fac5925714ab5304e
Ildana <ildana.ruzybayeva@gmail.com>
2020-11-23 13:40:02 +0000

    Added more tests

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -5,12 +5,12 @@ import data from "./data";
 
 const Content = () => {
   const CoffeList = data.map((obj) => (
-    <Grid item xs={12} sm={6} md={4}>
+    <Grid key={obj.id} item xs={12} sm={6} md={4} data-testid="single-item">
       <CoffeCard key={obj.id} info={obj.info} description={obj.description} />
     </Grid>
   ));
   return (
-    <Grid container spacing={4}>
+    <Grid container spacing={4} data-testid="list-item">
       {CoffeList}
     </Grid>
   );
